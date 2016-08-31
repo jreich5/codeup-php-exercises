1d596791c660046d402a798dc7f78e32a6476f1a
Justin Reich <justinreich.dev@gmail.com>
2016-08-31 14:02:33 -0500

    Refactored first two exercises and completed third exercise

--- foreach.php
+++ foreach.php
@@ -2,50 +2,24 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-
 foreach ($things as $thing) {
-    if (is_integer($thing)) {
-        echo 'integer' . PHP_EOL;
-    } else if (is_float($thing)) {
-        echo 'float' . PHP_EOL;
-    } else if (is_bool($thing)) {
-        echo 'bool' . PHP_EOL;
-    } else if (is_array($thing)) {
-        echo 'array' . PHP_EOL;
-    } else if (is_null($thing)) {
-        echo 'null' . PHP_EOL;
-    } else if (is_string($thing)) {
-        echo 'string' . PHP_EOL;
-    } else {
-        echo 'Type unknown.' . PHP_EOL;
-    }
+    echo gettype($thing) . PHP_EOL;
 }
 
 foreach ($things as $thing) {
     if (is_scalar($thing)) {
-        echo 'scalar' . PHP_EOL;
-        if (is_integer($thing)) {
-            echo 'integer' . PHP_EOL;
-        } else if (is_float($thing)) {
-            echo 'float' . PHP_EOL;
-        } else if (is_bool($thing)) {
-            echo 'bool' . PHP_EOL;
-        } else if (is_array($thing)) {
-            echo 'array' . PHP_EOL;
-        } else if (is_null($thing)) {
-            echo 'null' . PHP_EOL;
-        } else if (is_string($thing)) {
-            echo 'string' . PHP_EOL;
-        } else {
-            echo 'Type unknown.' . PHP_EOL;
-        }
-    } 
+        print_r($thing);
+        echo PHP_EOL;
+    }
 }
 
-// is_integer();
-// is_float();
-// is_bool();
-// is_array();
-// is_null();
-// is_string();
-
+foreach ($things as $thing) {
+    if (is_array($thing)) {
+        foreach ($thing as $thin) {
+            echo($thin);
+        }
+        echo PHP_EOL;  
+    } else {
+        echo $thing . PHP_EOL;
+    }
+}
