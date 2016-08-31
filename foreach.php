eb1bc19e4eaf8659be6c2c010c96c6bddf38ca2f
Justin Reich <justinreich.dev@gmail.com>
2016-08-31 12:28:00 -0500

    Completed first step

--- foreach.php
+++ foreach.php
@@ -2,9 +2,30 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-foreach ($things else $thing) {
-    echo[$thing];
+
+foreach ($things as $thing) {
+    if (is_integer($thing)) {
+        echo 'integer' . PHP_EOL;
+    } else if (is_float($thing)) {
+        echo 'float' . PHP_EOL;
+    } else if (is_bool($thing)) {
+        echo 'bool' . PHP_EOL;
+    } else if (is_array($thing)) {
+        echo 'array' . PHP_EOL;
+    } else if (is_null($thing)) {
+        echo 'null' . PHP_EOL;
+    } else if (is_string($thing)) {
+        echo 'string' . PHP_EOL;
+    } else {
+        echo 'Type unknown.' . PHP_EOL;
+    }
 }
 
 
+// is_integer();
+// is_float();
+// is_bool();
+// is_array();
+// is_null();
+// is_string();
 
