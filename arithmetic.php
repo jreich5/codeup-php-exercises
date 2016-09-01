c7dbe6e0bf1e9e21bade4595ce33eeabccececdc
Justin Reich <justinreich.dev@gmail.com>
2016-09-01 11:43:56 -0500

    Added modulus function

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,9 @@
 <?php
 
+$a = 20;
+
+$b = 2;
+
 function add($a, $b) {
     return ($a + $b);
 }
@@ -16,9 +20,14 @@ function divide($a, $b) {
     return ($a / $b);
 }
 
+function modulus($a, $b) {
+    return ($a % $b);
+}
+
 // Add code to test your functions here
 
 echo 'Result of add ' . add(10, 10) . PHP_EOL;
 echo 'Result of subtract ' . subtract(10, 10) . PHP_EOL;
 echo 'Result of multiply ' . multiply(10, 10) . PHP_EOL;
 echo 'Result of divide ' . divide(10, 10) . PHP_EOL;
+echo 'Result of modulus ' . modulus(11, 10) . PHP_EOL;
